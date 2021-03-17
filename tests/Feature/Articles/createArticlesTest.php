cf1fc03d8e416b70d956a41359233d1d7451d4fd
Ernesto Maya <emaya167@gmail.com>
2021-03-17 13:42:58 -0600

    create articles with auth

--- tests/Feature/Articles/createArticlesTest.php
+++ tests/Feature/Articles/createArticlesTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Feature\Articles;
 
 use App\Models\Article;
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Laravel\Sanctum\Sanctum;
 use Tests\TestCase;
 
 class createArticlesTest extends TestCase
@@ -12,13 +14,41 @@ class createArticlesTest extends TestCase
     /**
      * @test
      */
-    public function can_create_articles()
-    {
-        $article = factory(Article::class)->raw();
 
-        //verify if don't  exists any article on database
+
+    public function guests_cannot_create_articles()
+    {
+
+        $article = array_filter(factory(Article::class)->raw(['user_id' => null]));
+
+
+        $this->jsonApi()->content([
+            'data' => [
+                'type' => 'articles',
+                'attributes' =>  $article
+            ]
+        ])->post(route('api.v1.articles.create'))->assertStatus(401);
+
 
         $this->assertDatabaseMissing('articles', $article);
+    }
+
+
+
+
+    public function Authenticated_users_can_create_articles()
+    {
+        //create user
+        $user = factory(User::class)->create();
+
+
+        $article = array_filter(factory(Article::class)->raw(['user_id' => null]));
+
+        //verify if don't  exists any article on database
+        $this->assertDatabaseMissing('articles', $article);
+
+        Sanctum::actingAs($user);
+
 
         $this->jsonApi()->content([
             'data' => [
@@ -28,13 +58,21 @@ class createArticlesTest extends TestCase
         ])->post(route('api.v1.articles.create'))->assertCreated();
 
         //verify if article exists on database
-
-        $this->assertDatabaseHas('articles', $article);
+        $this->assertDatabaseHas('articles', [
+            'user_id' => $user->id,
+            'title' => $article['title'],
+            'slug' => $article['slug'],
+            'content' => $article['content'],
+        ]);
     }
 
     public function title_is_required()
     {
         $article = factory(Article::class)->raw(['title' => '']);
+
+
+
+        Sanctum::actingAs(factory(User::class)->create());
 
         $this->jsonApi()->content([
             'data' => [
@@ -52,6 +90,8 @@ class createArticlesTest extends TestCase
     {
         $article = factory(Article::class)->raw(['content' => '']);
 
+        Sanctum::actingAs(factory(User::class)->create());
+
         $this->jsonApi()->content([
             'data' => [
                 'type' => 'articles',
@@ -67,6 +107,8 @@ class createArticlesTest extends TestCase
     public function slug_is_required()
     {
         $article = factory(Article::class)->raw(['slug' => '']);
+
+        Sanctum::actingAs(factory(User::class)->create());
 
         $this->jsonApi()->content([
             'data' => [
@@ -86,6 +128,8 @@ class createArticlesTest extends TestCase
         factory(Article::class)->create(['slug' => 'same-slug']);
 
         $article = factory(Article::class)->raw(['slug' => 'same-slug']);
+
+        Sanctum::actingAs(factory(User::class)->create());
 
         $this->jsonApi()->content([
             'data' => [
